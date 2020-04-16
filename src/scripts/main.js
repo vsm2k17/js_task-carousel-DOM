29f5dfce483fdd4d57f8314869447b841a1243db
Nick <nikolyaoooyartchuk@gmail.com>
2020-04-16 20:25:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const btnPrev = document.querySelector('.carousel__btn_prev');
+const btnNext = document.querySelector('.carousel__btn_next');
+const carousel = document.querySelector('.carousel');
+const dots = document.querySelector('.carousel__dots-wrap');
+
+let position = 0;
+let indexOfActiveDot = 0;
+
+btnNext.addEventListener('click', (e) => {
+  if (position === -1050) {
+    return;
+  }
+  carousel.style.left = `${position - 350}px`;
+  position -= 350;
+  dots.children[indexOfActiveDot].classList.remove('carousel__dot_active');
+  dots.children[indexOfActiveDot + 1].classList.add('carousel__dot_active');
+  indexOfActiveDot++;
+});
+
+btnPrev.addEventListener('click', (e) => {
+  if (position === 0) {
+    return;
+  }
+  carousel.style.left = `${position + 350}px`;
+  position += 350;
+  dots.children[indexOfActiveDot].classList.remove('carousel__dot_active');
+  dots.children[indexOfActiveDot - 1].classList.add('carousel__dot_active');
+  indexOfActiveDot--;
+});
